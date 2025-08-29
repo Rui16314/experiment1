2f165042a70eddc3be1b73a11d593d5af3de2dbe
Rui16314 <rui315@yorku.ca>
2025-08-29 13:31:53 -0400

    Update init.py

--- results_dashboard/init.py
+++ results_dashboard/init.py
@@ -1,19 +1,24 @@
 from otree.api import *
 
+
 doc = """
-Results dashboard showing experiment data and comparisons
+Results Dashboard for Data Analysis
 """
+
 
 class C(BaseConstants):
     NAME_IN_URL = 'results_dashboard'
     PLAYERS_PER_GROUP = None
     NUM_ROUNDS = 1
 
+
 class Subsession(BaseSubsession):
     pass
 
+
 class Group(BaseGroup):
     pass
+
 
 class Player(BasePlayer):
     pass
